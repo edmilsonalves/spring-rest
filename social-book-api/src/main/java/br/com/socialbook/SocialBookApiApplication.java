5207ea9076ef9a48862ad2602464f75a498b1c5c
edmilson.reis <edmilson.reis@BRPRDKT002589.valueteam.com.br>
2016-05-31 14:28:27 -0300

    Primeiro commit rest

---
+++ social-book-api/src/main/java/br/com/socialbook/SocialBookApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.socialbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialBookApiApplication.class, args);
+	}
+}
